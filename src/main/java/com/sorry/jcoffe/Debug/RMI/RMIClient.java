657d7905885962e5b694967cbcba1690ea746cf8
RE0 <sev7enman@gmail.com>
2021-01-19 00:36:45 +0800

    RMI

--- src/main/java/com/sorry/jcoffe/Debug/RMI/RMIClient.java
+++ src/main/java/com/sorry/jcoffe/Debug/RMI/RMIClient.java
@@ -1,32 +1,16 @@
 package com.sorry.jcoffe.Debug.RMI;
 
+import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class RMIClient{
     public static void main(String[] args) throws Exception{
-        /*
-    Exception in thread "main" java.rmi.ConnectIOException: error during JRMP connection establishment; nested exception is:
-        java.net.SocketException: Connection reset
-        at sun.rmi.transport.tcp.TCPChannel.createConnection(TCPChannel.java:307)
-        at sun.rmi.transport.tcp.TCPChannel.newConnection(TCPChannel.java:202)
-        at sun.rmi.server.UnicastRef.invoke(UnicastRef.java:129)
-        at java.rmi.server.RemoteObjectInvocationHandler.invokeRemoteMethod(RemoteObjectInvocationHandler.java:227)
-        at java.rmi.server.RemoteObjectInvocationHandler.invoke(RemoteObjectInvocationHandler.java:179)
-        at com.sun.proxy.$Proxy0.Exploit(Unknown Source)
-        at com.sorry.jcoffe.Debug.RMI.RMIClient.main(RMIClient.java:10)
-    Caused by: java.net.SocketException: Connection reset
-        at java.net.SocketInputStream.read(SocketInputStream.java:210)
-        at java.net.SocketInputStream.read(SocketInputStream.java:141)
-        at java.io.BufferedInputStream.fill(BufferedInputStream.java:246)
-        at java.io.BufferedInputStream.read(BufferedInputStream.java:265)
-        at java.io.DataInputStream.readByte(DataInputStream.java:265)
-        at sun.rmi.transport.tcp.TCPChannel.createConnection(TCPChannel.java:246)
-        ... 6 more
-         */
-        Registry registry = LocateRegistry.getRegistry("39.108.244.167",1099);
-        EXP exp = (EXP) registry.lookup("EXP");
+        //Registry registry = LocateRegistry.getRegistry("39.108.244.167",1099);
+        //EXP exp = (EXP) registry.lookup("EXP");
+        EXP exp = (EXP) Naming.lookup("rmi://:1099/exp");
         exp.Exploit("open /System/Applications/Calculator.app");
+        System.out.println(exp);
 
     }
 }
--- src/main/java/com/sorry/jcoffe/Debug/RMI/RMIServer.java
+++ src/main/java/com/sorry/jcoffe/Debug/RMI/RMIServer.java
@@ -1,14 +1,25 @@
 package com.sorry.jcoffe.Debug.RMI;
 
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
 
-public class RMIServer {
+public class RMIServer extends UnicastRemoteObject implements EXP{
+
+    protected RMIServer() throws RemoteException {
+    }
+
+    @Override
+    public void Exploit(String cmd) throws RemoteException {
+
+    }
+
     public static void main(String[] args) throws RemoteException {
         try{
             LocateRegistry.createRegistry(1099);
-            LocateRegistry.getRegistry("localhost",1099).bind("EXP",new EXPImpl());
-            System.out.println("EXP RMIServer Ready!");
+            Naming.bind("rmi://:1099/exp",new EXPImpl());
+            System.out.println("EXP RMIServer started.");
         }catch (Exception e){
             //todo nothing
         }
