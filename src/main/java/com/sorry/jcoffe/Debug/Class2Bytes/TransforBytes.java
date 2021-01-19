6fa3a5d88e71211944197dcd081ba18c8d665b02
RE0 <sev7enman@gmail.com>
2021-01-19 12:22:30 +0800

    Transfor

--- src/main/java/com/sorry/jcoffe/Debug/Class2Bytes/TransforBytes.java
+++ src/main/java/com/sorry/jcoffe/Debug/Class2Bytes/TransforBytes.java
@@ -1,40 +1,38 @@
 package com.sorry.jcoffe.Debug.Class2Bytes;
 
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
 import java.io.*;
 
-public class ClassLoaderMain {
+public class TransforBytes {
     public static void main(String[] args) throws Exception {
-        byte[] bs = getBytesByFile("/Users/0x22cb7139/programing/java/spring/spring/target/classes/com/sorry/saury/Test/Calculator.class");
-        /*
-        for (int i = 0; i < bs.length; i++) {
-            System.out.print(bs[i]+",");
-        }
-         */
-        sun.misc.BASE64Encoder encoder;
-        encoder = new sun.misc.BASE64Encoder();
-        System.out.println(encoder.encode(bs));
 
+        //BASE64Encoder encoder = new BASE64Encoder();
         //byte[] bs = sun.misc.BASE64Decoder("yv66vgAAADIANAoABwAlCgAmACcIACgKACYAKQcAKgoABQAlBwArAQAGPGluaXQ+AQADKClWAQAEQ29kZQEAD0xpbmVOdW1iZXJUYWJsZQEAEkxvY2FsVmFyaWFibGVUYWJsZQEABHRoaXMBAAtManNvbi9UZXN0OwEACkV4Y2VwdGlvbnMHACwBAAl0cmFuc2Zvcm0BAKYoTGNvbS9zdW4vb3JnL2FwYWNoZS94YWxhbi9pbnRlcm5hbC94c2x0Yy9ET007TGNvbS9zdW4vb3JnL2FwYWNoZS94bWwvaW50ZXJuYWwvZHRtL0RUTUF4aXNJdGVyYXRvcjtMY29tL3N1bi9vcmcvYXBhY2hlL3htbC9pbnRlcm5hbC9zZXJpYWxpemVyL1NlcmlhbGl6YXRpb25IYW5kbGVyOylWAQAIZG9jdW1lbnQBAC1MY29tL3N1bi9vcmcvYXBhY2hlL3hhbGFuL2ludGVybmFsL3hzbHRjL0RPTTsBAAhpdGVyYXRvcgEANUxjb20vc3VuL29yZy9hcGFjaGUveG1sL2ludGVybmFsL2R0bS9EVE1BeGlzSXRlcmF0b3I7AQAHaGFuZGxlcgEAQUxjb20vc3VuL29yZy9hcGFjaGUveG1sL2ludGVybmFsL3NlcmlhbGl6ZXIvU2VyaWFsaXphdGlvbkhhbmRsZXI7AQByKExjb20vc3VuL29yZy9hcGFjaGUveGFsYW4vaW50ZXJuYWwveHNsdGMvRE9NO1tMY29tL3N1bi9vcmcvYXBhY2hlL3htbC9pbnRlcm5hbC9zZXJpYWxpemVyL1NlcmlhbGl6YXRpb25IYW5kbGVyOylWAQAIaGFuZGxlcnMBAEJbTGNvbS9zdW4vb3JnL2FwYWNoZS94bWwvaW50ZXJuYWwvc2VyaWFsaXplci9TZXJpYWxpemF0aW9uSGFuZGxlcjsHAC0BAARtYWluAQAWKFtMamF2YS9sYW5nL1N0cmluZzspVgEABGFyZ3MBABNbTGphdmEvbGFuZy9TdHJpbmc7AQABdAcALgEAClNvdXJjZUZpbGUBAAlUZXN0LmphdmEMAAgACQcALwwAMAAxAQAEY2FsYwwAMgAzAQAJanNvbi9UZXN0AQBAY29tL3N1bi9vcmcvYXBhY2hlL3hhbGFuL2ludGVybmFsL3hzbHRjL3J1bnRpbWUvQWJzdHJhY3RUcmFuc2xldAEAE2phdmEvaW8vSU9FeGNlcHRpb24BADljb20vc3VuL29yZy9hcGFjaGUveGFsYW4vaW50ZXJuYWwveHNsdGMvVHJhbnNsZXRFeGNlcHRpb24BABNqYXZhL2xhbmcvRXhjZXB0aW9uAQARamF2YS9sYW5nL1J1bnRpbWUBAApnZXRSdW50aW1lAQAVKClMamF2YS9sYW5nL1J1bnRpbWU7AQAEZXhlYwEAJyhMamF2YS9sYW5nL1N0cmluZzspTGphdmEvbGFuZy9Qcm9jZXNzOwAhAAUABwAAAAAABAABAAgACQACAAoAAABAAAIAAQAAAA4qtwABuAACEgO2AARXsQAAAAIACwAAAA4AAwAAABEABAASAA0AEwAMAAAADAABAAAADgANAA4AAAAPAAAABAABABAAAQARABIAAQAKAAAASQAAAAQAAAABsQAAAAIACwAAAAYAAQAAABcADAAAACoABAAAAAEADQAOAAAAAAABABMAFAABAAAAAQAVABYAAgAAAAEAFwAYAAMAAQARABkAAgAKAAAAPwAAAAMAAAABsQAAAAIACwAAAAYAAQAAABwADAAAACAAAwAAAAEADQAOAAAAAAABABMAFAABAAAAAQAaABsAAgAPAAAABAABABwACQAdAB4AAgAKAAAAQQACAAIAAAAJuwAFWbcABkyxAAAAAgALAAAACgACAAAAHwAIACAADAAAABYAAgAAAAkAHwAgAAAACAABACEADgABAA8AAAAEAAEAIgABACMAAAACACQ=");
-
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] shirocheck = decoder.decodeBuffer(args[1]);
+        CreateFileFromBytes(shirocheck,args[0]);
     }
-    public static byte[] getBytesByFile(String pathStr) {
-        File file = new File(pathStr);
-        try {
-            FileInputStream fis = new FileInputStream(file);
-            ByteArrayOutputStream bos = new ByteArrayOutputStream(1024);
-            byte[] b = new byte[1024];
-            int n;
-            while ((n = fis.read(b)) != -1) {
-                bos.write(b, 0, n);
-            }
-            fis.close();
-            byte[] data = bos.toByteArray();
-            bos.close();
-            return data;
-        } catch (Exception e) {
-            e.printStackTrace();
+
+    public static void CreateFileFromBytes(byte[] expbyte,String targetname) throws IOException {
+        FileOutputStream fos=new FileOutputStream(targetname);
+        fos.write(expbyte);
+        fos.close();
+    }
+
+    public static byte[] BytesFromFile(String path) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(1024);
+        FileInputStream fis = new FileInputStream(path);
+        byte[] b= new byte[1024];
+        int n;
+        while ((n = fis.read(b)) != -1) {
+            bos.write(b, 0, n);
         }
-        return null;
+        fis.close();
+        byte[] data = bos.toByteArray();
+        bos.close();
+        return data;
     }
 
 }
--- src/main/java/com/sorry/jcoffe/Debug/Serialization/Serialize.java
+++ src/main/java/com/sorry/jcoffe/Debug/Serialization/Serialize.java
@@ -13,13 +13,13 @@ public class Serialize {
         System.out.println(test);
         */
         try {
-            //Serilizesec120();
-            Unserilizasec120();
+            //Serializesec120();
+            Unserializesec120();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static void Serilizesec120() throws Exception {
+    public static void Seriliazesec120() throws Exception {
         sec120 test = new sec120();
         test.setLocation("HNUST");
         test.setMembers(new String[]{"zzq", "jjj","shm"});
@@ -29,7 +29,7 @@ public class Serialize {
         System.out.println(oos);
 
     }
-    public static void Unserilizasec120() throws Exception{
+    public static void Unserializesec120() throws Exception{
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("sec120.ser")));
         sec120 unsec120 = (sec120) ois.readObject();
         sec120 aaa = new sec120();
