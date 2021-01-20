0b3a07411d02a771c9b85c64796e11f57a5462a9
RE0 <sev7enman@gmail.com>
2021-01-21 00:08:40 +0800

    nothing

--- src/main/java/com/sorry/jcoffe/Debug/RMI/EXPImpl.java
+++ src/main/java/com/sorry/jcoffe/Debug/RMI/EXPImpl.java
@@ -3,6 +3,7 @@ package com.sorry.jcoffe.Debug.RMI;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -13,15 +14,22 @@ public class EXPImpl extends UnicastRemoteObject implements EXP {
     @Override
     public String Exploit(String cmd) throws IOException {
 
-        //ProcessBuilder processBuilder = new ProcessBuilder("/bin/sh","-c",cmd);
         Process p;
+        String result="";
         if (System.getProperty("os.name").toLowerCase().indexOf("windows") >= 0) {
             p = Runtime.getRuntime().exec(new String[]{"cmd.exe", "/c", cmd});
         } else {
             p = Runtime.getRuntime().exec(cmd);
         }
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p.getInputStream(),"GB2312"));
-        return bufferedReader.readLine();
+        String disr = bufferedReader.readLine();
+        String result2 = result;
+        while(disr != null){
+            String result3 = result2 + disr + "\n";
+            disr = bufferedReader.readLine();
+            result2 =result3;
+        }
+        return result2;
 
     }
 }
